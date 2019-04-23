a66cd7201cb11484a59be0f5a8549b23cc5a94d9
ANKAIAH.NANDYALA <ANKAIAH.NANDYALA@LAPTOP-FV63L6VC>
2019-04-23 11:07:06 +0530

    success

---
+++ src/com/app/Ankaiahshivani.java
@@ -0,0 +1,21 @@
+package com.app;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+/**
+ * Servlet implementation class Ankaiahshivani
+ */
+@WebServlet("/Ankaiahshivani")
+public class Ankaiahshivani extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Ankaiahshivani() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+}
